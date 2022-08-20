bab4f8501491f13ede64d98d559c200ae6a9c60f
ragnar <diegocsgow@gmail.com>
2022-08-20 11:30:04 -0500

    actualizacion

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -11,8 +11,24 @@ function hallar()
 {
     var x = (peso.value);
     var y = (talla.value);
+
+    var imc = x/(y * y);
     
-    alert("tu IMC es " + parseInt(x/(y * y)));
+    alert("tu IMC es " + Math.round(imc));
+
+    if(imc <= 18.5){
+        alert("Estas Bajo de peso");
+    }
+
+    if(imc >= 18.5 && imc <= 24.9){
+        alert("Estas en un peso adecuado");
+    }
+    if(imc >= 25 && imc <= 29.9){
+        alert("Estas en sobrepeso");
+    }
+    if(imc >= 30){
+        alert("Tu IMC indica que estas en obesidad");
+    }
 }
 
 function buscar()
@@ -21,8 +37,24 @@ function buscar()
     var y = (talla2.value);
     //Talla final para poder realizar el calculo 
     tF = y/100; 
+    //Calculo de IMC
     imc = x/(tF * tF);
-
     alert("tu IMC es " + parseInt(imc));
+
+    if(imc <= 18.5){
+        alert("Estas Bajo de peso");
+    }
+
+    if(imc >= 18.5 && imc <= 24.9){
+        alert("Estas en un peso adecuado");
+    }
+    if(imc >= 25 && imc <= 29.9){
+        alert("Estas en sobrepeso");
+    }
+    if(imc >= 30){
+        alert("Tu IMC indica que estas en obesidad");
+    }
 }
+
+
 
